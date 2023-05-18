78e989677024d7c4547bfa7b181172aefb56214f
Vadim Zeitlin <vadim@wxwidgets.org>
2023-05-19 01:39:02 +0200

    Fix creating Chinese locales under macOS
    Creating Chinese locales such as zh_TW didn't work because this string
    didn't appear in [NSLocale availableLocaleIdentifiers] return value,
    as only the variants explicitly specifying the script were included.
    
    Fix this by ignoring the script part when checking if the locale is
    available. This should work well for zh_TW or zh_SG, which exist only in
    a single script (traditional and simplified, respectively), but it's not
    really clear whether it works, or what should we do if it doesn't work,
    for zh_CN or zh_HK both of which exist in both scripts.
    
    See #23209.

--- src/osx/core/uilocale.mm
+++ src/osx/core/uilocale.mm
@@ -20,6 +20,7 @@
 
 #if wxUSE_INTL
 
+#include "wx/log.h"
 #include "wx/uilocale.h"
 #include "wx/private/uilocale.h"
 
@@ -76,7 +77,7 @@ public:
     {
     }
 
-    ~wxUILocaleImplCF() wxOVERRIDE
+    ~wxUILocaleImplCF() override
     {
         [m_nsloc release];
     }
@@ -86,28 +87,59 @@ public:
         // Surprisingly, localeWithLocaleIdentifier: always succeeds, even for
         // completely invalid strings, so we need to check if the name is
         // actually in the list of the supported locales ourselves.
-        static wxCFRef<CFArrayRef>
-            all((CFArrayRef)[[NSLocale availableLocaleIdentifiers] retain]);
+        bool isAvailable = false;
+        for ( id nsLocId in [NSLocale availableLocaleIdentifiers] )
+        {
+            // We can't simply compare the names here because the list returned
+            // by NSLocale is incomplete and doesn't contain all synonyms, e.g.
+            // it only contains "zh_Hant_TW" but not "zh_TW" itself, so we need
+            // to do our own matching.
+            auto strId = wxCFStringRef::AsString(nsLocId);
+
+            // Currently FromTag() doesn't deal with "lang_script_region"
+            // format and only supports "lang-script_region", but can parse
+            // both "lang_region" and "lang-region" correctly, so ensure we
+            // pass it something it can deal with.
+            strId.Replace("_", "-", false /* only the first one */);
+
+            const auto availId = wxLocaleIdent::FromTag(strId);
+            if ( availId.IsEmpty() )
+            {
+                wxLogDebug("Failed to parse locale identifier \"%s\"", strId);
+                continue;
+            }
+
+            // We ignore the other components because we consider that locale
+            // creation will succeed even if they don't match exactly, but the
+            // language and the region must match (even though the latter one
+            // might be empty).
+            if ( availId.GetLanguage() == locId.GetLanguage() &&
+                    availId.GetRegion() == locId.GetRegion() )
+            {
+                isAvailable = true;
+                break;
+            }
+        }
+
+        if ( !isAvailable )
+            return nullptr;
 
         wxCFStringRef cfName(locId.GetName());
-        if ( ![(NSArray*)all.get() containsObject: cfName.AsNSString()] )
-            return NULL;
-
         auto nsloc = [NSLocale localeWithLocaleIdentifier: cfName.AsNSString()];
         if ( !nsloc )
-            return NULL;
+            return nullptr;
 
         return new wxUILocaleImplCF(nsloc);
     }
 
-    void Use() wxOVERRIDE;
-    wxString GetName() const wxOVERRIDE;
-    wxLocaleIdent GetLocaleId() const wxOVERRIDE;
-    wxString GetInfo(wxLocaleInfo index, wxLocaleCategory cat) const wxOVERRIDE;
-    wxString GetLocalizedName(wxLocaleName name, wxLocaleForm form) const wxOVERRIDE;
-    wxLayoutDirection GetLayoutDirection() const wxOVERRIDE;
+    void Use() override;
+    wxString GetName() const override;
+    wxLocaleIdent GetLocaleId() const override;
+    wxString GetInfo(wxLocaleInfo index, wxLocaleCategory cat) const override;
+    wxString GetLocalizedName(wxLocaleName name, wxLocaleForm form) const override;
+    wxLayoutDirection GetLayoutDirection() const override;
     int CompareStrings(const wxString& lhs, const wxString& rhs,
-                       int flags) const wxOVERRIDE;
+                       int flags) const override;
 
 private:
     NSLocale* const m_nsloc;
