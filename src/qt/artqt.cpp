526b64f65f6a59ffdb5e83b648a6017ef683a238
ali kettab <aliket1435@gmail.com>
2026-02-20 00:01:11 +0100

    wxQt: Return theme icon if there is no standard icon corresponding to wxArtID

--- src/qt/artqt.cpp
+++ src/qt/artqt.cpp
@@ -26,7 +26,8 @@
 namespace // anonymous
 {
 // This helper function tries to return the standard icon QStyle::StandardPixmap
-// corresponding to the wxArtID if it exist.
+// corresponding to the wxArtID if it exists. It tries to return the corresponding
+// theme icon using QIcon::fromTheme() function otherwise.
 QIcon GetQtIconFromArtID(const wxArtID& id)
 {
     QStyle::StandardPixmap standardIcon;
@@ -90,6 +91,80 @@ QIcon GetQtIconFromArtID(const wxArtID& id)
     {
         return QApplication::style()->standardIcon(standardIcon);
     }
+
+    // Try returning theme icon if there is no corresponding standard icon:
+
+#define TRY_RETURN_THEMEICON(themeId)           \
+    {                                           \
+        if ( QIcon::hasThemeIcon(themeId) )     \
+            return QIcon::fromTheme(themeId);   \
+    }
+
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 7, 0))
+    #define TRY_RETURN_THEMEICON2(themeId, unused) \
+        return QIcon::fromTheme(QIcon::ThemeIcon::themeId);
+#else // Qt version < 6.7
+    #define TRY_RETURN_THEMEICON2(unused, themeId) TRY_RETURN_THEMEICON(themeId)
+#endif // Qt version >= 6.7
+
+    if ( id == wxART_ADD_BOOKMARK )
+        TRY_RETURN_THEMEICON2(ListAdd, "list-add")
+    else if ( id == wxART_DEL_BOOKMARK )
+        TRY_RETURN_THEMEICON2(ListRemove, "list-remove")
+    else if ( id == wxART_HELP_SETTINGS )
+        TRY_RETURN_THEMEICON("preferences-desktop-font")
+    else if ( id == wxART_HELP_FOLDER )
+        TRY_RETURN_THEMEICON("folder")
+    else if ( id == wxART_HELP_PAGE )
+        TRY_RETURN_THEMEICON("text-x-generic")
+    else if ( id == wxART_GOTO_FIRST )
+        TRY_RETURN_THEMEICON("go-first")
+    else if ( id == wxART_GOTO_LAST )
+        TRY_RETURN_THEMEICON("go-last")
+    else if ( id == wxART_FILE_SAVE_AS )
+        TRY_RETURN_THEMEICON2(DocumentSaveAs, "document-save-as")
+    else if ( id == wxART_PRINT )
+        TRY_RETURN_THEMEICON2(DocumentPrint, "document-print")
+    else if ( id == wxART_TIP )
+        TRY_RETURN_THEMEICON2(DialogInformation, "dialog-information")
+    else if ( id == wxART_REMOVABLE )
+        TRY_RETURN_THEMEICON2(MediaFlash, "drive-removable-media")
+    else if ( id == wxART_EXECUTABLE_FILE )
+        TRY_RETURN_THEMEICON("system-run")
+    else if ( id == wxART_MISSING_IMAGE )
+        TRY_RETURN_THEMEICON2(ImageMissing, "image-missing")
+    else if ( id == wxART_COPY )
+        TRY_RETURN_THEMEICON2(EditCopy, "edit-copy")
+    else if ( id == wxART_CUT )
+        TRY_RETURN_THEMEICON2(EditCut, "edit-cut")
+    else if ( id == wxART_PASTE )
+        TRY_RETURN_THEMEICON2(EditPaste, "edit-paste")
+    else if ( id == wxART_DELETE )
+        TRY_RETURN_THEMEICON2(EditDelete, "edit-delete")
+    else if ( id == wxART_NEW )
+        TRY_RETURN_THEMEICON2(DocumentNew, "document-new")
+    else if ( id == wxART_UNDO )
+        TRY_RETURN_THEMEICON2(EditUndo, "edit-undo")
+    else if ( id == wxART_REDO )
+        TRY_RETURN_THEMEICON2(EditRedo, "edit-redo")
+    else if ( id == wxART_PLUS )
+        TRY_RETURN_THEMEICON2(ListAdd, "list-add")
+    else if ( id == wxART_MINUS )
+        TRY_RETURN_THEMEICON2(ListRemove, "list-remove")
+    else if ( id == wxART_QUIT )
+        TRY_RETURN_THEMEICON2(ApplicationExit, "application-exit")
+    else if ( id == wxART_FIND )
+        TRY_RETURN_THEMEICON2(EditFind, "edit-find")
+    else if ( id == wxART_FIND_AND_REPLACE )
+        TRY_RETURN_THEMEICON("edit-find-replace")
+    else if ( id == wxART_FULL_SCREEN )
+        TRY_RETURN_THEMEICON2(ViewFullscreen, "view-fullscreen")
+    else if ( id == wxART_EDIT )
+        TRY_RETURN_THEMEICON2(InsertText, "accessories-text-editor")
+//  else if ( id == wxART_HELP_SIDE_PANEL )
+//  else if ( id == wxART_HELP_BOOK )
+//  else if ( id == wxART_GO_DIR_UP )
+//  else if ( id == wxART_REPORT_VIEW )
 
     return QIcon();
 }
